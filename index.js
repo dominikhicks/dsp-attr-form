adbe854f1555d3b720f3703dfaa47f9d44569fbc
dominikhicks <161826764+dominikhicks@users.noreply.github.com>
2025-06-16 13:12:23 +0200

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const survey = new Survey.Model(json);
-survey.applyTheme(SurveyTheme.DefaultLightPanelless);
-survey.onComplete.add((sender, options) => {
-    console.log(JSON.stringify(sender.data, null, 3));
-});
-$("#surveyElement").Survey({ model: survey });
